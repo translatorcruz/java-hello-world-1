11c09f8eef28239f2fce0be6ab8fa4d930b01f1c
NVC <nunovicentecruz@outlook.com>
2021-01-11 12:55:01 +0000

    Adicionada segunda alteração no Main

--- src/pt/serro/test/Main.java
+++ src/pt/serro/test/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	System.out.println("First change");
+	System.out.println("Second change");
     }
 }
